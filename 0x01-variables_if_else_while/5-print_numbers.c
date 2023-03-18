6da5b39eec732f7bb762dd5407f383fcbc17fc71
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-17 18:34:33 -0700

    com

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ *
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
