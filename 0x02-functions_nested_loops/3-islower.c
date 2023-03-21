6007bb07255fc6085c3ce681b1403a66474da7d7
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-21 04:22:10 -0700

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
  * _islower - check lowecase.
- *
+ * @c: letter being tested
  * Return: int (1) or (0)
  */
 int _islower(int c)
 {
-	return c >= 97 && c <= 122 ? 1 : 0;
+	return (c >= 97 && c <= 122 ? 1 : 0);
 }
 
