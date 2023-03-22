333b8de9c6d96f06c6f192c111ecd1f00974c434
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-21 17:55:09 -0700

    0

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _isalpha - check if char is alpha
- * @c: check char
+ * print_last_digit - print
+ * @c: check int
  * Return: int
  */
-int _isalpha(int c)
+int print_last_digit(int c)
 {
-	return ((c >= 97 && c <= (97 + 25)) || (c >= 65 || c < (65 + 25)) ? 1 : 0);
+	return (c % 10);
 }
 
 
