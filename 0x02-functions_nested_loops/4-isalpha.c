adc3cdfc2beaeefc11a0756e3462b255c7c56ce4
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-21 17:20:16 -0700

    0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <main.h>
+
+/**
+ * _isalpha - check if char is alpha
+ * @c: check char
+ * Return: int
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= (97 + 25)) || (c >= 65 || c < (65 + 25)) ? 1 : 0);
+}
+
