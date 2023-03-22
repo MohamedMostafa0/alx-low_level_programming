9149e08680f4f6a92d33c475b45dc32f7c3e8132
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-21 18:09:43 -0700

    0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,33 @@
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * _isalpha - check if char is alpha
- * @c: check char
- * Return: int
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
  */
-int _isalpha(int c)
+void print_to_98(int n)
 {
-	return ((c >= 97 && c <= (97 + 25)) || (c >= 65 || c < (65 + 25)) ? 1 : 0);
-}
+	int i, j;
 
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
 
