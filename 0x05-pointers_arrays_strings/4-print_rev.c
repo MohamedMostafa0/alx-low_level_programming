e065e97ad677aa6f3f52155525ede5aefd467d13
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-28 05:16:32 -0700

    2

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code.
+ * @s: param
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(*s), i;
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
+
