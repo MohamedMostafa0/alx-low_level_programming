e46168fa0c1627489acaeeb86fd12cbd8e19c4c2
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-28 15:15:49 -0700

    3

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - func
+ * @str: string param
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 == 0)
+		i /= 2;
+	else
+		i = (i - 1) / 2;
+
+	for (; str[i] != '\0')
+		_putchar(str[i]);
+
+	_putchar(str[i]);
+}
+
