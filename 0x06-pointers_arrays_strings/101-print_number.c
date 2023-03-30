c65c182b3ea685da5b05f0578d47f6b7db38bbbd
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-03-30 14:47:52 -0700

    0

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,13 +6,20 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
-		_putchar('-');
+	unsigned int n1;
 
-	while (n > 0)
+	n1 = n;
+
+	if (n < 0)
 	{
-		_putchar((n % 10) + '0');
-		n /= 10;
+		_putchar('-');
+		n1 = -n;
 	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
 
