5ed6db1e91f7dce52b3317c49fb13f18b4a902be
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-04-03 15:18:42 -0700

    0

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,48 @@
-#include "main.h"
+#include "main.hi"
 
 /**
- *  - func
- * @a: a - Variable
+ * _strspn - func
+ * @s: a - Variable
+ * @accept: b - variable
  * Return: return
  */
-
+unsigned int _strspn(char *s, char *accept)
 {
+	char a[265] = {0}
+	int i;
+	unsigned int res = 0;
 
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (accept[i] >= 'a' && accept[i] <= 'z')
+		{
+			a[accept[i] - 'a'] += 1;
+		}
+		else if (accept[i] >= 'A' && accept[i] <= 'Z')
+		{
+			a[accept[i] - 'A' + 26] += 1;
+		}
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		int flag = -1;
+
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			flag = s[i] - 'a';
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			flag = s[i] - 'A' + 26;
+		}
+		if (flag > -1 && a[flag] > 0)
+		{
+			res++;
+		}
+		else
+			break;
+	}
+	return (res);
 }
 
