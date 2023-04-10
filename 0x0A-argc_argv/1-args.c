0c84d3d2c18ff0186f67518e214dafa12717c403
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-04-09 17:31:13 -0700

    0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main func
+ * @argc: a
+ * @argv: b array
+ * Return: int
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
