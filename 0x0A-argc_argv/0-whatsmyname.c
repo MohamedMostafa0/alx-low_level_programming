096fe5b34c7ffe5eda9edee8044499c8f34cde78
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-04-09 17:19:29 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,12 @@
  * main - main func
  * @argc: a
  * @argv: b array
+ * Return: int
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
 
