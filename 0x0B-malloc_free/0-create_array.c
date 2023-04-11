37555b6e1cde2650d2cf6f6c415bef9cbf4c0b46
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-04-11 01:05:47 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 
-	if (size == 0 || arr = NULL)
+	if (size == 0 || arr == NULL)
 		return (NULL);
 	return (arr);
 }
