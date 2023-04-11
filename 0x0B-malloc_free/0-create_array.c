c48f11ca88a23d1ff36e6b3901226579ea0ab10d
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-04-11 01:04:34 -0700

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - check the code.
+ * @size: param a
+ * @c: param b
+ * Return: array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	arr = malloc(sizeof(char), size);
+
+	if (size == 0 || arr = NULL)
+		return (NULL);
+	return (arr);
+}
+
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifndef MAINH
+#define MAINH
+char *create_array(unsigned int size, char c);
+#endif
