d928aeb518da9035878c07ab18e4128effbe85e7
MohamedMostafa0 <moh.dyedrock@gmail.com>
2023-04-11 01:07:55 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	int i;
 
 	arr = malloc(sizeof(char) * size);
 
 	if (size == 0 || arr == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 	return (arr);
 }
 
